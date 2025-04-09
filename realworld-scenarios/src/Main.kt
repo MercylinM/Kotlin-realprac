de6ffe92db8183c6f639bf311e37ca72ff3cc7e9
mercylin muthoni <muthonimercylin@gmail.com>
2025-04-09 13:17:46 +0300

    real world scenariosls

---
+++ realworld-scenarios/src/Main.kt
@@ -0,0 +1,63 @@
+fun main() {
+    val result = organizeWallet(arrayOf(100, 50, 200, 1000, 20, 100))
+    println(result.contentToString())
+    println(isPalindromic("Hannah"))
+    println(isPalindromic("Mariam"))
+
+    println(estimateShippingCost(36,42,2200, 1800, 16.75))
+
+    println(isPasswordValid("123wordpass"))
+    println(isPasswordValid("123password"))
+    println(isPasswordValid("1253wordpass"))
+}
+
+fun organizeWallet(notes: Array<Int>): Array<Int>{
+    return notes.sortedArray()
+}
+
+fun isPalindromic(name: String): Boolean{
+//    return name.lowercase() === name.lowercase().reversed()
+    val lname = name.lowercase()
+    var l = 0
+    var r = lname.length-1
+
+    while (l < r){
+        if (lname[l] == lname[r]){
+            l++
+            r--
+        }
+        else{
+            return false
+        }
+    }
+    return true
+}
+
+fun estimateShippingCost(diameterA: Int, diameterB: Int, numA: Int, numB: Int,rate: Double): Double{
+    val pi = 3.14159
+    val radiusA = diameterA / 200.0
+    val radiusB = diameterB / 200.0
+    val typeAVol = (4.0/3) * pi * radiusA * radiusA * radiusA * numA
+    val typeBVol = (4.0/3) * pi * radiusB * radiusB * radiusB * numB
+
+    val totalVal = typeAVol + typeBVol
+    return totalVal * rate
+}
+
+fun calcSphereVolume(radius: Double): Double{
+    val pi = 3.14159
+    return  (4/3) * pi * radius * radius * radius
+}
+
+fun isPasswordValid(userPassword: String): Boolean{
+    var badpass = "password123".lowercase()
+    badpass = badpass.toCharArray().sorted().joinToString("")
+    val sortedPassword = userPassword.lowercase().toCharArray().sorted().joinToString("")
+
+    return badpass != sortedPassword
+//    if(badpass == sortedPassword){
+//        return false
+//    }else{
+//        return true
+//    }
+}
